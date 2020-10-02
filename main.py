40bc0c868428b4242db61247f3e8e4e388d0b374
sim-my <simranpanthi101@gmail.com>
2020-10-02 12:53:55 +0545

    Added speec recognizer

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from gtts import gTTS
 import speech_recognition as sr
 from playsound import playsound
-
 #function to convert text to speech and play the sound
 def talk(audio):
     for line in audio.splitlines():
-        text_to_speech = gTTS(text=audio, lang='en-uk')
+        text_to_speech = gTTS(text=audio, lang='en-us')
         text_to_speech.save('audio.mp3')
         playsound("audio.mp3")
+def myCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Say')
+        audio = r.listen(source)
+    try: 
+        command = r.recognize_google(audio).lower()
+        talk('You said: ' + command + '\n')
+    except sr.UnknownValueError:
+        talk('Your last command couldn\'t be heard')
 
-talk('Hey! I am Easy Dictionary.')
+myCommand()
+
