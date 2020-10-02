57f27b52ae73e05dd6b40c9fbc3d4feef01e751e
sim-my <simranpanthi101@gmail.com>
2020-10-02 18:56:28 +0545

    Added comments to the code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#import the necessary libraries
 import requests
 from difflib import get_close_matches 
 from gtts import gTTS
@@ -5,13 +6,15 @@ import speech_recognition as sr
 from playsound import playsound
 import os
 
-def talk(audio):    
+#converts text to audio
+def talk(text):    
     for line in audio.splitlines():
         text_to_speech = gTTS(text=audio, lang='en-uk')
         text_to_speech.save('audio.mp3')
         playsound("audio.mp3")
         os.remove("audio.mp3")
 
+#gives the definitions of the word
 def translate(word):
     talk('The meanings of'+word+'are')
     response = requests.get('https://api.dictionaryapi.dev/api/v2/entries/en/'+word)
@@ -19,10 +22,7 @@ def translate(word):
     for definition in data[0]['meanings'][0]['definitions']:
         talk(definition["definition"])
 
-
-
-
-#function to convert text to speech and play the sound
+#Asks for command
 def dictionaryCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -35,9 +35,12 @@ def dictionaryCommand():
         askMeaningOrNOt()
     return command
 
+#welcome greeting
 def welcome():
     talk('Welcome to easy dictionary')
 
+
+#Yes or No for asking meaning
 def askMeaningOrNOt():
     talk('Do you wanna ask meaning of any word? Answer in yes or no.')
     answer = dictionaryCommand()
@@ -49,14 +52,18 @@ def askMeaningOrNOt():
         talk('I\'m sorry. I don\'t know what to do.')
         askMeaningOrNOt()
 
+#Intermediate function for generating definition of given word
 def giveWord():
     talk('Say the word')
     word = dictionaryCommand()
     translate(word)
     askMeaningOrNOt()
 
+
+#exits from the application
 def exit():
     talk('Thanks for using easy dictionary')
 
+#calling necessary functions
 welcome()
 askMeaningOrNOt()
