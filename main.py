66ad4835f38da9b20dae91c7250f3ba415483067
sim-my <simranpanthi101@gmail.com>
2020-10-02 14:40:12 +0545

    Fixed bug with audio file

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from gtts import gTTS
 import speech_recognition as sr
 from playsound import playsound
+import os
 #function to convert text to speech and play the sound
 def talk(audio):
     for line in audio.splitlines():
         text_to_speech = gTTS(text=audio, lang='en-us')
         text_to_speech.save('audio.mp3')
         playsound("audio.mp3")
+        os.remove("audio.mp3")
+
 def myCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print('Say')
+        talk('Say a word')
         audio = r.listen(source)
     try: 
         command = r.recognize_google(audio).lower()
